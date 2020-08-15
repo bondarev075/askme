3c96b3bebdfb37b55d2322bdfdc10e2e9d5add48
bondarev075 <bondarev075@yandex.ru>
2020-08-15 17:06:54 +0700

    Add autologin after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user[:id]
       redirect_to root_url, notice: 'Вы успешно зарегистрированы!'
     else
       render :new
