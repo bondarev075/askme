dbd92ef05ea5b6823789aa9a5ef84ac0062f7ba1
bondarev075 <bondarev075@yandex.ru>
2020-08-15 12:49:48 +0700

    Questions controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "users#index"
   resources :users, except: [:destroy]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :questions
+  resources :questions, except: [:index, :show, :new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'users' => 'users#show'
 
