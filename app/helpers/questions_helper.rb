e63a75adf8f1e07bd016e0b46125190b95ad2e73
bondarev075 <bondarev075@yandex.ru>
2020-08-19 20:19:23 +0700

    Add author to question

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,10 @@
 module QuestionsHelper
+  def question_author(question)
+    User.find(question.author) if question.author
+  end
+
+  def question_author_name(question)
+    user = question_author(question)
+    user.name if user
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_120828) do
+ActiveRecord::Schema.define(version: 2020_08_18_142750) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_08_17_120828) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
+    t.integer "author"
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
