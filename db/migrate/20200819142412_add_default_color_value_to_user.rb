7710515cd8505769c3d9dcb8fc44520a62b15a06
bondarev075 <bondarev075@yandex.ru>
2020-08-21 19:54:48 +0700

    Change migration to set default color to existing users

--- db/migrate/20200819142412_add_default_color_value_to_user.rb
+++ db/migrate/20200819142412_add_default_color_value_to_user.rb
@@ -2,4 +2,8 @@ class AddDefaultColorValueToUser < ActiveRecord::Migration[6.0]
   def change
     change_column_default :users, :profile_bg_color, from: nil, to: "#005a55"
   end
+
+  reversible do |dir|
+    dir.up { User.where(profile_bg_color: nil).update_all(profile_bg_color: '#005a55') }
+  end
 end
